334627c1318ae6573e5847c9d5929dab2dbf8c20
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-08-26 14:27:44 -0500

    Some initial items for nullboard interop

---
+++ nullboard_gitlab.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3 -i
+# Copyright 2022, Collabora, Ltd.
+#
+# SPDX-License-Identifier: BSL-1.0
+#
+# Author: Ryan Pavlik <ryan.pavlik@collabora.com>
+
+import re
+from typing import Any, Callable, Dict, List
+
+from work_item_and_collection import WorkUnit, WorkUnitCollection
+
+_REF_RE = re.compile(r"^([!#][0-9]+)\b.*")
+
+
+class ListName:
+    TODO = "TODO"
+    DONE = "Done"
+    DOING = "Coding"
+    REVIEW = "Needs Review"
+
+
+def guess_list(item: WorkUnit) -> str:
+    if item.key_item.state in ("merged", "closed"):
+        return ListName.DONE
+    mr = item.get_key_item_as_mr()
+    if mr:
+        # merge request
+        if mr.work_in_progress:
+            return ListName.DOING
+        if "Needs Action" in mr.labels:
+            return ListName.DOING
+    return ListName.TODO
+
+
+def make_note_text(item: WorkUnit) -> str:
+    return "{}: {} {}\n{}".format(
+        item.ref,
+        item.title,
+        item.web_url,
+        "\n".join(item.make_mr_url_list()),
+    )
+
+
+def update_board(
+    work: WorkUnitCollection,
+    board: Dict[str, Any],
+    note_text_maker: Callable[[WorkUnit], str] = make_note_text,
+    list_guesser: Callable[[WorkUnit], str] = guess_list,
+):
+    """Update the JSON data for a nullboard kanban board"""
+
+    # the key item ref for all items used to update an existing note
+    updated = set()
+
+    # Go through all existing lists
+    for notelist in board["lists"]:
+        list_name = notelist["title"]
+        print("In %s" % list_name)
+
+        # For each item in those lists, extract the ref, and update the text if we can
+        for note in notelist["notes"]:
+            m = _REF_RE.match(note["text"])
+            if not m:
+                # Can't find a reference to an item at the start of the text
+                print("Could not find an entry for '%s'" % note["text"])
+                continue
+
+            ref = m.group(1)
+            print("Found note for item %s" % ref)
+            item = work.items_by_ref.get(ref)
+            if not item:
+                # We don't know about this item
+                continue
+
+            new_text = note_text_maker(item)
+            note["text"] = new_text
+            print("Updated text")
+            updated.add(item.ref)
+            item.list_name = list_name
+
+    # Decide what list to put the leftovers in
+    all_new: Dict[str, List[Dict[str, str]]] = {}
+
+    for item in work.items:
+        if item.ref in updated:
+            # we already did this
+            continue
+        print("New item for %s" % item.title)
+        note = {"text": note_text_maker(item)}
+        list_name = item.list_name or list_guesser(item)
+        if list_name not in all_new:
+            all_new[list_name] = []
+        all_new[list_name].append(note)
+
+    # Now go through the lists in the json and add the appropriate new items
+    for notelist in board["lists"]:
+        title = notelist["title"]
+        if title in all_new:
+            notelist["notes"].extend(all_new[title])
+            print("Added new items to", title)
---
+++ work_item_and_collection.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3 -i
+# Copyright 2022, Collabora, Ltd.
+#
+# SPDX-License-Identifier: BSL-1.0
+#
+# Author: Ryan Pavlik <ryan.pavlik@collabora.com>
+
+import dataclasses
+from typing import Any, Dict, List, Optional, Union, cast
+
+import gitlab
+import gitlab.v4.objects
+
+from gitlab.v4.objects import ProjectIssue, ProjectMergeRequest
+
+
+def make_url_list_item(issue_or_mr) -> str:
+    return "- {}: {} {}".format(
+        issue_or_mr.references["short"], issue_or_mr.title, issue_or_mr.web_url
+    )
+
+
+@dataclasses.dataclass
+class WorkUnit:
+    key_item: Union[ProjectIssue, ProjectMergeRequest]
+
+    mrs: List[ProjectMergeRequest] = dataclasses.field(default_factory=list)
+
+    list_name: Optional[str] = None
+
+    @property
+    def ref(self):
+        return self.key_item.references["short"]
+
+    @property
+    def title(self):
+        return self.key_item.title
+
+    @property
+    def web_url(self):
+        return self.key_item.web_url
+
+    @property
+    def is_mr(self):
+        return "merge_status" in self.key_item.attributes
+
+    def get_key_item_as_mr(self) -> Optional[ProjectMergeRequest]:
+        if not self.is_mr:
+            return None
+        return cast(ProjectMergeRequest, self.key_item)
+
+    def get_key_item_as_issue(self) -> Optional[ProjectIssue]:
+        if self.is_mr:
+            return None
+        return cast(ProjectIssue, self.key_item)
+
+    def make_url_list(self):
+        yield make_url_list_item(self.key_item)
+
+    def make_mr_url_list(self):
+        for mr in self.mrs:
+            yield make_url_list_item(mr)
+
+
+@dataclasses.dataclass
+class WorkUnitCollection:
+    items_by_ref: Dict[str, WorkUnit] = {}
+    items: List[WorkUnit] = []
+
+    def _add_item(
+        self,
+        api_item: Union[ProjectIssue, ProjectMergeRequest],
+    ) -> Optional[WorkUnit]:
+        short_ref = api_item.references["short"]
+        if short_ref in self.items_by_ref:
+            return
+        item = WorkUnit(key_item=api_item)
+        self.items.append(item)
+        self.items_by_ref[short_ref] = item
+        print(short_ref, api_item.title)
+        return item
+
+    def add_issue(self, proj: gitlab.v4.objects.Project, issue: ProjectIssue):
+        item = self._add_item(issue)
+        if not item:
+            return
+        # TODO closed_by instead?
+        for mr_dict in issue.related_merge_requests():
+            mr_num: int = mr_dict["iid"]  # type: ignore
+            mr = proj.mergerequests.get(mr_num)
+            item.mrs.append(mr)
+
+            mr_short_ref = mr.references["short"]
+            self.items_by_ref[mr_short_ref] = item
+            print("->", mr_short_ref)
+
+    def add_mr(self, _proj: gitlab.v4.objects.Project, mr: ProjectMergeRequest):
+        item = self._add_item(mr)
+        if not item:
+            return
+        # TODO combine other stuff?
