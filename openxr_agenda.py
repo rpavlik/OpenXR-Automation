583c7ff29dd706893240458958be1fea1311be66
Ryan Pavlik <ryan.pavlik@gmail.com>
2022-09-08 11:49:09 -0500

    Further improve

--- openxr_agenda.py
+++ openxr_agenda.py
@@ -8,7 +8,6 @@
 import json
 import logging
 import os
-import re
 from dataclasses import dataclass, field
 from enum import Enum
 from typing import Dict, Iterable, List, Optional, Set, Union, cast
@@ -21,14 +20,16 @@ from gitlab.v4.objects import ProjectIssue, ProjectMergeRequest
 
 from nullboard_gitlab import parse_board
 from openxr_release_checklist_update import ListName
-from work_item_and_collection import WorkUnit, WorkUnitCollection, get_short_ref
+from work_item_and_collection import WorkUnit, WorkUnitCollection, get_short_ref, is_mr
 
 load_dotenv()
+
+EXPIRE_AFTER_SECONDS = 60 * 10
 
 _SKIP_DISCUSSING_MR_LABELS = ("Objection Window", "Needs Action")
 _SKIP_DISCUSSING_ISSUE_LABELS = ["Needs Merge Request"]
 _SKIP_DISCUSSING_MILESTONES = ["OpenXR-Next Preliminary"]
-_SKIP_DRAFTS_WITHOUT_THESE_LABELS = ["Needs Discussion"]
+_SKIP_DRAFTS_WITHOUT_THESE_LABELS = ["Needs Discussion", "Needs Approval"]
 
 
 class Labels:
@@ -43,6 +44,7 @@ _SKIP_DISCUSSING_LABELS = (
     "Conformance-Next",
     "For Contractor",
     "Stale",
+    "OpenXR 1.1",
     "Phoenix-F2F",  # next f2f
 )
 
@@ -69,7 +71,7 @@ class Column(Enum):
     COMBINED_AUTHOR_ASSIGNEE = 5
     THUMBS = 6
     LABELS = 7
-    TASK_COMPLETION_STATUS = 8
+    STATUS = 8
     # NUM_COMMENTS = 8
 
     @classmethod
@@ -78,6 +80,7 @@ class Column(Enum):
             cls.INDENT,
             cls.REF,
             cls.TITLE,
+            cls.STATUS,
             cls.AUTHOR,
             cls.ASSIGNEE,
             cls.THUMBS,
@@ -90,6 +93,7 @@ class Column(Enum):
             cls.INDENT,
             cls.REF,
             cls.TITLE,
+            cls.STATUS,
             cls.COMBINED_AUTHOR_ASSIGNEE,
             cls.LABELS,
         )
@@ -100,7 +104,7 @@ class Column(Enum):
             cls.INDENT,
             cls.REF,
             cls.TITLE,
-            cls.TASK_COMPLETION_STATUS,
+            cls.STATUS,
             cls.COMBINED_AUTHOR_ASSIGNEE,
             cls.THUMBS,
             cls.LABELS,
@@ -124,8 +128,8 @@ class Column(Enum):
             return "Thumbs"
         if col == cls.LABELS:
             return "Labels"
-        if col == cls.TASK_COMPLETION_STATUS:
-            return "Tasks"
+        if col == cls.STATUS:
+            return "Status"
         raise RuntimeError("Unrecognized column " + str(col))
 
 
@@ -171,10 +175,13 @@ def _get_col_for_issue_or_mr(
     if col == Column.LABELS:
         return "{}".format(", ".join(issue_or_mr.labels))
 
-    if col == Column.TASK_COMPLETION_STATUS:
+    if col == Column.STATUS:
+        status = issue_or_mr.state
+        if is_mr(issue_or_mr):
+            return status
         if not issue_or_mr.has_tasks:
-            return ""
-        return issue_or_mr.task_status
+            return status
+        return "{}, {}".format(status, issue_or_mr.task_status)
     # if col == Columns.NUM_COMMENTS:
     #     return
     raise RuntimeError("Not handled!")
@@ -201,48 +208,6 @@ def _format_issue_or_mr_for_table(
     return _make_table_row(col_contents)
 
 
-def _format_issue_or_mr(
-    issue_or_mr: Union[ProjectIssue, ProjectMergeRequest], include_author: bool = True
-):
-    elements = [
-        "[{}]({})".format(issue_or_mr.references["short"], issue_or_mr.web_url),
-        "-",
-        issue_or_mr.title,
-    ]
-
-    # Indicate the author/assignee as applicable and requested
-    author = None
-    if include_author:
-        author = _format_user_dict(issue_or_mr.author)
-    assignee = None
-    if issue_or_mr.assignee:
-        assignee = _format_user_dict(issue_or_mr.assignee)
-
-    if author and assignee and author == assignee:
-        elements.append("(Author/assignee: {})".format(author))
-    else:
-        if author:
-
-            elements.append("(Author: {})".format(author))
-        if assignee:
-            elements.append("(Assignee: {}".format(assignee))
-
-    # Thumbs
-    if issue_or_mr.upvotes or issue_or_mr.downvotes:
-        elements.append(
-            "({}{})".format(
-                ":+1:" * issue_or_mr.upvotes, ":-1:" * issue_or_mr.downvotes
-            )
-        )
-    else:
-        elements.append("(no thumbs yet)")
-
-    # Labels
-    if issue_or_mr.labels:
-        elements.append("(Labels: {})".format(", ".join(issue_or_mr.labels)))
-    return " ".join(elements)
-
-
 @dataclass
 class Agenda:
     khr_release_checklists_composing: List[WorkUnit] = field(default_factory=list)
@@ -259,7 +224,43 @@ class Agenda:
     )
     ext_and_vendor_other: List[WorkUnit] = field(default_factory=list)
 
+    do_filter_out: bool = True
+
     _listed_key_refs: Set[str] = field(default_factory=set)
+
+    def add_to_list(
+        self, item: WorkUnit, item_list: List[WorkUnit], skip_if_any_skippable=False
+    ) -> bool:
+        log = logging.getLogger(__name__)
+        if self.is_on_agenda(item):
+            log.info(
+                "Skipping because already on the agenda: %s: %s",
+                item.ref,
+                item.title,
+            )
+            return False
+        if self.do_filter_out:
+            skippable = tuple(
+                should_skip_discussion_of_issue_or_mr(issue_or_mr)
+                for issue_or_mr in item.all_issues_and_mrs()
+            )
+            if skip_if_any_skippable and any(skippable):
+                log.info(
+                    "Skipping because at least one issue/mr was skippable: %s: %s",
+                    item.ref,
+                    item.title,
+                )
+                return False
+            if all(skippable):
+                log.info(
+                    "Skipping because all relevant issue/mr were skippable: %s: %s",
+                    item.ref,
+                    item.title,
+                )
+                return False
+        item_list.append(item)
+        self.record_on_agenda(item)
+        return True
 
     def is_on_agenda(self, item: WorkUnit) -> bool:
         return item.ref in self._listed_key_refs
@@ -317,23 +318,11 @@ class Agenda:
         return "\n".join(lines)
 
     def _release_checklist_section(self, name: str, work: List[WorkUnit]) -> str:
-        if not work:
-            return ""
-
-        lines = [
-            "## Release checklists: {}".format(name),
-            "",
-        ]
-
-        for item in work:
-
-            lines.append(
-                "* {}".format(_format_issue_or_mr(item.key_item, include_author=False))
-            )
-            for issue_or_mr in item.non_key_issues_and_mrs():
-                lines.append("  * {}".format(_format_issue_or_mr(issue_or_mr)))
-        lines.append("")
-        return "\n".join(lines)
+        return self._normal_section(
+            "Release checklists: {}".format(name),
+            work,
+            Column.columns_for_release_checklists(),
+        )
 
 
 def should_skip_discussion_of_issue_or_mr(
@@ -355,7 +344,7 @@ def should_skip_discussion_of_issue_or_mr(
 
         # Don't skip all drafts, but skip those that aren't specially marked.
         if mr.draft or mr.work_in_progress:
-            if not labels.isdisjoint(_SKIP_DRAFTS_WITHOUT_THESE_LABELS):
+            if labels.isdisjoint(_SKIP_DRAFTS_WITHOUT_THESE_LABELS):
                 return True
     else:
         # We have an issue
@@ -391,7 +380,9 @@ def maybe_create_item_for_issue(
         return None
     refs = [ref]
     refs.extend(
-        "!{}".format(mr_dict.iid) for mr_dict in issue.related_merge_requests().values()
+        # type data is wrong
+        "!{}".format(mr_dict["iid"])  # type: ignore
+        for mr_dict in issue.related_merge_requests()
     )
 
     return work.add_refs(proj, refs, {ref: issue})
@@ -420,7 +411,6 @@ def main(in_nbx_filename, out_md_filename):
     log = logging.getLogger(__name__)
     work = WorkUnitCollection()
 
-    EXPIRE_AFTER_SECONDS = 180
     session = requests_cache.CachedSession(
         cache_name="gitlab_cache", expire_after=EXPIRE_AFTER_SECONDS
     )
@@ -442,30 +432,25 @@ def main(in_nbx_filename, out_md_filename):
     parse_board(proj, work, existing_board)
     agenda = Agenda()
 
-    do_filter_out = True
-    # print(work.items[0].key_item.pformat())
     for item in work.items:
-        if do_filter_out:
-            if all(
-                should_skip_discussion_of_issue_or_mr(issue_or_mr)
-                for issue_or_mr in item.all_issues_and_mrs()
-            ):
-                print("Skipping", item.ref, item.title)
-                continue
+
         if item.list_name == ListName.INITIAL_COMPOSITION:
             if is_khr(item):
-                agenda.khr_release_checklists_composing.append(item)
+                agenda.add_to_list(item, agenda.khr_release_checklists_composing)
             else:
-                agenda.ext_and_vendor_release_checklists_composing.append(item)
-            agenda.record_on_agenda(item)
+                agenda.add_to_list(
+                    item, agenda.ext_and_vendor_release_checklists_composing
+                )
             continue
 
         if item.list_name in _REVIEW_LISTS:
             if is_khr(item):
-                agenda.khr_release_checklists_reviews.append(item)
+                agenda.add_to_list(item, agenda.khr_release_checklists_reviews)
             else:
-                agenda.ext_and_vendor_release_checklists_reviews.append(item)
-            agenda.record_on_agenda(item)
+
+                agenda.add_to_list(
+                    item, agenda.ext_and_vendor_release_checklists_reviews
+                )
             continue
 
     log.info("Looking for open issues from GitHub")
@@ -474,21 +459,7 @@ def main(in_nbx_filename, out_md_filename):
         state="opened", label="From GitHub", iterator=True
     ):
         issue = cast(ProjectIssue, gh_issue)
-        # ref = get_short_ref(issue)
-        # if ref in work.items_by_ref:
-        #     # Can stop here without going further
-        #     continue
-        # refs = [ref]
-        # refs.extend(
-        #     "!{}".format(mr_dict["iid"]) for mr_dict in issue.related_merge_requests()
-        # )
-        # item = work.add_or_get_item_for_refs(proj, refs, {ref: issue})
 
-        # try:
-        #     work.add_related_mrs_to_issue_workunit(proj, item)
-        # except:
-        #     # Ignore errors in related MRs
-        #     pass
         item = maybe_create_item_for_issue(proj, work, issue)
         if not item:
             # We already had this in the work collection
@@ -502,91 +473,53 @@ def main(in_nbx_filename, out_md_filename):
             )
             continue
 
-        if do_filter_out:
-            if any(
-                should_skip_discussion_of_issue_or_mr(issue_or_mr)
-                for issue_or_mr in item.all_issues_and_mrs()
-            ):
-                log.info("Skipping github issue %s: %s", item.ref, item.title)
-                continue
         if is_khr(item):
-            agenda.other_issues.append(item)
+            agenda.add_to_list(item, agenda.other_issues, skip_if_any_skippable=True)
         else:
-            agenda.ext_and_vendor_other.append(item)
-        agenda.record_on_agenda(item)
+            agenda.add_to_list(
+                item, agenda.ext_and_vendor_other, skip_if_any_skippable=True
+            )
 
     # Grab issues to discuss
     for label in _DISCUSSION_TAGS:
         log.info("Looking for open issues labeled %s", label)
         for issue in proj.issues.list(state="opened", label=label, iterator=True):
-            # try:
-            #     item = work.add_issue(proj, cast(ProjectIssue, issue), False)
-            # except:
-            #     continue
-
-            # if not item:
-            #     # we already handled it
-            #     continue
-
-            # try:
-            #     work.add_related_mrs_to_issue_workunit(proj, item)
-            # except:
-            #     # Ignore errors in related MRs
-            #     pass
-
-            # continue
 
             item = maybe_create_item_for_issue(proj, work, cast(ProjectIssue, issue))
             if not item:
                 # We already had this in the work collection
                 continue
+
             if agenda.is_on_agenda(item):
                 log.info(
                     "Discussion-labeled issue %d is already on the agenda as %s",
                     issue.iid,
                     item.ref,
                 )
-            if do_filter_out:
-                if any(
-                    should_skip_discussion_of_issue_or_mr(issue_or_mr)
-                    for issue_or_mr in item.all_issues_and_mrs()
-                ):
-                    log.info("Skipping 'other' issue %s: %s", item.ref, item.title)
-                    continue
 
             if is_khr(item):
-                agenda.other_issues.append(item)
+                agenda.add_to_list(
+                    item, agenda.other_issues, skip_if_any_skippable=True
+                )
             else:
-                agenda.ext_and_vendor_other.append(item)
-            agenda.record_on_agenda(item)
+                agenda.add_to_list(
+                    item, agenda.ext_and_vendor_other, skip_if_any_skippable=True
+                )
 
         log.info("Looking for open merge requests labeled %s", label)
         for mr in proj.mergerequests.list(state="opened", label=label, iterator=True):
-            # try:
-            #     item = work.add_mr(proj, cast(ProjectMergeRequest, mr))
-            # except:
-            #     continue
-            # if not item:
-            #     # we already handled it
-            #     continue
-
             item = maybe_create_item_for_mr(proj, work, cast(ProjectMergeRequest, mr))
+
             if not item:
                 # We already had this in the work collection
                 continue
-            if do_filter_out:
-                if any(
-                    should_skip_discussion_of_issue_or_mr(issue_or_mr)
-                    for issue_or_mr in item.all_issues_and_mrs()
-                ):
-                    log.info("Skipping 'other' MR %s: %s", item.ref, item.title)
-                    continue
 
             if is_khr(item):
-                agenda.other_mrs.append(item)
+                agenda.add_to_list(item, agenda.other_mrs, skip_if_any_skippable=True)
             else:
-                agenda.ext_and_vendor_other.append(item)
-            agenda.record_on_agenda(item)
+                agenda.add_to_list(
+                    item, agenda.ext_and_vendor_other, skip_if_any_skippable=True
+                )
 
     with open(out_md_filename, "w", encoding="utf-8") as fp:
         fp.write(str(agenda))
