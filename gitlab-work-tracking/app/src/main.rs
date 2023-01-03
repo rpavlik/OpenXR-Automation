a565e7ef31bc12c31fa5e782dce361418d9acba9
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-01-03 17:00:21 -0600

    wip

--- gitlab-work-tracking/app/src/main.rs
+++ gitlab-work-tracking/app/src/main.rs
@@ -1,10 +1,16 @@
-use std::path::Path;
+use std::{
+    collections::{hash_map::Entry, HashMap, HashSet},
+    iter::zip,
+    path::Path,
+};
 
 use anyhow::{anyhow, Error};
 use clap::Parser;
 use dotenvy::dotenv;
 use gitlab_work::{note::NoteLine, ProjectItemReference, UnitId, WorkUnitCollection};
-use log::info;
+use itertools::{repeat_n, Itertools};
+use log::{info, warn};
+use nullboard_tools::{GenericList, GenericLists};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -35,6 +41,45 @@ fn parse_note(s: &str) -> Vec<LineOrGitlabRef> {
 struct ProcessedNote {
     unit_id: Option<UnitId>,
     lines: Vec<LineOrGitlabRef>,
+    deleted: bool,
+}
+
+fn parse_and_process_note(collection: &mut WorkUnitCollection, s: &str) -> ProcessedNote {
+    let lines: Vec<_> = s
+        .split("\n")
+        .map(NoteLine::parse_line)
+        .map(|l| {
+            if let Some(reference) = l.reference {
+                LineOrGitlabRef::GitlabRef(reference)
+            } else {
+                LineOrGitlabRef::FreeformText(l.line)
+            }
+        })
+        .collect();
+
+    let refs: Vec<ProjectItemReference> = lines
+        .iter()
+        .filter_map(|line| match line {
+            LineOrGitlabRef::FreeformText(_) => None,
+            LineOrGitlabRef::GitlabRef(reference) => Some(reference),
+        })
+        .cloned()
+        .collect();
+
+    let unit_id = if refs.is_empty() {
+        None
+    } else {
+        let result = collection.add_or_get_unit_for_refs(refs);
+        if let Err(e) = &result {
+            warn!("Problem calling add/get unit for refs: {}", e);
+        }
+        result.ok()
+    };
+    ProcessedNote {
+        unit_id,
+        lines,
+        deleted: false,
+    }
 }
 
 fn process_note(
@@ -57,6 +102,7 @@ fn process_note(
     Ok(ProcessedNote {
         unit_id,
         lines: note,
+        deleted: false,
     })
 }
 
@@ -79,13 +125,53 @@ fn main() -> Result<(), anyhow::Error> {
 
     let board = nullboard_tools::Board::load_from_json(path)?;
 
-    let notes_iter = board.lists.iter().flat_map(|list| list.notes.iter());
+    // let notes_iter = board
+    //     .lists
+    //     .iter()
+    //     .flat_map(|list| zip(repeat_n(list, list.notes.len()), list.notes.iter()));
     let mut collection = WorkUnitCollection::default();
-    for note in notes_iter {
-        let lines = parse_note(&note.text);
-        let note = process_note(&mut collection, lines)?;
-        info!("{:?}", note);
+    let mut parsed_lists = vec![];
+    for list in board.lists {
+        parsed_lists.push(list.map_note_text(|text| parse_and_process_note(&mut collection, text)));
     }
+    let mut units_handled: HashMap<UnitId, ()> = Default::default();
+    for list in parsed_lists.iter_mut() {
+        for note in &mut list.notes {
+            if let Some(id) = &note.text.unit_id {
+                let (id, _) = collection.get_unit_following_extinction(*id, 5)?;
+                match units_handled.entry(id) {
+                    Entry::Occupied(_) => {
+                        note.text.deleted = true;
+                        warn!(
+                            "Deleting note because its work unit was already handled: {} {:?}",
+                            id, note.text.lines
+                        );
+                    }
+                    Entry::Vacant(e) => {
+                        note.text.unit_id = Some(id);
+                        e.insert(());
+                    }
+                }
+            }
+        }
+    }
+    // let mut updated_lists = vec![];
+    // for list in parsed_lists {
+    //     updated_lists.push(list.notes.into_iter().unique_by(|n| n.t))
+    // }
+    // let mut processed_lists = vec![];
+    // let mut collection = WorkUnitCollection::default();
+    // for list in parsed_lists.0.iter().rev() {
+    //     /// generate units in right-to-left list order
+    //     processed_lists.push(list.map_note_text(|lines| process_note(&mut collection, lines)))
+    // }
+
+    // (board.lists.iter().map(|list| GenericList { title: list.title.clone(),notes: }))
+    // for note in notes_iter {
+    //     let lines = parse_note(&note.text);
+    //     let note = process_note(&mut collection, lines)?;
+    //     info!("{:?}", note);
+    // }
 
     Ok(())
 }
--- gitlab-work-tracking/nullboard/src/lib.rs
+++ gitlab-work-tracking/nullboard/src/lib.rs
@@ -95,6 +95,19 @@ impl Board {
     pub fn get_revision(&self) -> u32 {
         self.revision
     }
+
+    pub fn make_new_revision_with_lists(&self, lists: Vec<GenericList<String>>) -> Self {
+        let mut ret = Self {
+            format: self.format,
+            id: self.id,
+            revision: self.revision,
+            title: self.title.clone(),
+            lists: lists.into_iter().map(|l| List::from(l)).collect(),
+            history: self.history.clone(),
+        };
+        ret.increment_revision();
+        ret
+    }
 }
 
 impl Default for Board {
@@ -129,6 +142,183 @@ impl Default for Note {
             raw: false,
             min: false,
         }
+    }
+}
+
+/// Nullboard list note, with arbitrary text type
+#[derive(Debug)]
+pub struct GenericNote<T>
+where
+    T: core::fmt::Debug,
+{
+    /// Contents of the note
+    pub text: T,
+    /// Whether the note is shown "raw" (without a border, makes it look like a sub-header)
+    pub raw: bool,
+    /// Whether the note is shown minimized/collapsed
+    pub min: bool,
+}
+
+impl From<GenericNote<String>> for Note {
+    fn from(note: GenericNote<String>) -> Self {
+        Self {
+            text: note.text,
+            raw: note.raw,
+            min: note.min,
+        }
+    }
+}
+
+impl<T: std::fmt::Debug> GenericNote<T> {
+    // pub fn new_from_text_and_note_flags(text: T, old_note: &Note) -> Self {
+    //     GenericNote {
+    //         text,
+    //         raw: old_note.raw,
+    //         min: old_note.min,
+    //     }
+    // }
+
+    pub fn with_replacement_text<U: std::fmt::Debug>(&self, text: U) -> GenericNote<U> {
+        GenericNote {
+            text,
+            raw: self.raw,
+            min: self.min,
+        }
+    }
+}
+
+impl Note {
+    pub fn with_replacement_text<T: std::fmt::Debug>(&self, text: T) -> GenericNote<T> {
+        GenericNote {
+            text,
+            raw: self.raw,
+            min: self.min,
+        }
+    }
+
+    pub fn map_text<T: std::fmt::Debug, F: Fn(&str) -> T>(&self, f: F) -> GenericNote<T> {
+        self.with_replacement_text(f(&self.text))
+    }
+}
+
+impl From<Note> for GenericNote<String> {
+    fn from(note: Note) -> Self {
+        Self {
+            text: note.text,
+            raw: note.raw,
+            min: note.min,
+        }
+    }
+}
+
+/// A structure representing a list in a board as exported to JSON from Nullboard, with arbitrary note text type
+#[derive(Debug)]
+pub struct GenericList<T: core::fmt::Debug> {
+    /// Title of the list
+    pub title: String,
+    /// Notes in the list
+    pub notes: Vec<GenericNote<T>>,
+}
+
+impl From<GenericList<String>> for List {
+    fn from(list: GenericList<String>) -> Self {
+        Self {
+            title: list.title,
+            notes: list.notes.into_iter().map(|n| Note::from(n)).collect(),
+        }
+    }
+}
+
+impl From<List> for GenericList<String> {
+    fn from(list: List) -> Self {
+        Self {
+            title: list.title,
+            notes: list
+                .notes
+                .into_iter()
+                .map(|n| GenericNote::from(n))
+                .collect(),
+        }
+    }
+}
+
+// -- map_note_text --//
+
+fn map_generic_note_text<T: std::fmt::Debug, B: std::fmt::Debug>(
+    mut f: impl FnMut(&T) -> B,
+) -> impl FnMut(&GenericNote<T>) -> GenericNote<B> {
+    move |note| GenericNote {
+        text: f(&note.text),
+        raw: note.raw,
+        min: note.min,
+    }
+}
+impl<T: core::fmt::Debug> GenericList<T> {
+    pub fn map_note_text<U: std::fmt::Debug, F: FnMut(&T) -> U>(&self, f: F) -> GenericList<U> {
+        // let mut f = f;
+        GenericList {
+            title: self.title.clone(),
+            notes: self
+                .notes
+                .iter()
+                // .map(move |note| note.map_text(f))
+                .map(map_generic_note_text(f))
+                .collect(),
+        }
+    }
+}
+
+fn map_note_text<B: std::fmt::Debug>(
+    mut f: impl FnMut(&str) -> B,
+) -> impl FnMut(&Note) -> GenericNote<B> {
+    move |note| GenericNote {
+        text: f(&note.text),
+        raw: note.raw,
+        min: note.min,
+    }
+}
+
+impl List {
+    pub fn map_note_text<T: std::fmt::Debug, F: FnMut(&str) -> T>(&self, f: F) -> GenericList<T> {
+        // let mut f = f;
+        GenericList {
+            title: self.title.clone(),
+            notes: self
+                .notes
+                .iter()
+                // .map(move |note| note.map_text(f))
+                .map(map_note_text(f))
+                .collect(),
+        }
+    }
+}
+
+// fn map_generic_list<T: std::fmt::Debug, B: std::fmt::Debug>(
+//     mut f: impl FnMut(&GenericList<T>) -> GenericList<B>,
+// ) -> impl FnMut(&GenericList<T>) -> GenericList<B> {
+//     // let mut map_note = ;
+//     let f_ref = &f;
+//     |list| list.map_note_text(f_ref)
+//     // GenericList {
+//     //     title: list.title.clone(),
+//     //     // notes: list.notes.iter().map(map_generic_note_text(f)).collect(),
+//     //     notes: list.map_note_text(f)
+//     // }
+// }
+
+/// A structure representing the lists in a board, with arbitrary note type
+#[derive(Debug, Default)]
+pub struct GenericLists<T: core::fmt::Debug>(pub Vec<GenericList<T>>);
+
+impl<T: core::fmt::Debug> GenericLists<T> {
+    pub fn new() -> Self {
+        Self(Default::default())
+    }
+}
+
+impl From<GenericLists<String>> for Vec<List> {
+    fn from(lists: GenericLists<String>) -> Self {
+        lists.0.into_iter().map(|l| List::from(l)).collect()
     }
 }
 
