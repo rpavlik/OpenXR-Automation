7b71d27f916dc33f30b40a916507d667581cac37
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-01-05 09:18:45 -0600

    Improve output

--- gitlab-work-tracking/app/src/main.rs
+++ gitlab-work-tracking/app/src/main.rs
@@ -11,7 +11,9 @@ use board_update::{
 };
 use clap::{Args, Parser};
 use dotenvy::dotenv;
+use env_logger::Env;
 use gitlab_work::{ProjectMapper, WorkUnitCollection};
+use log::info;
 use nullboard_tools::map_note_data_in_lists;
 use std::path::{Path, PathBuf};
 
@@ -48,63 +50,74 @@ struct Cli {
     gitlab: GitlabArgs,
 }
 
+fn compute_default_output_filename(path: &Path) -> Result<PathBuf, anyhow::Error> {
+    path.file_stem()
+        .map(|p| {
+            let mut p = p.to_owned();
+            p.push(".updated.nbx");
+            p
+        })
+        .map(|file_name| path.with_file_name(file_name))
+        .ok_or_else(|| anyhow!("Could not get file stem"))
+}
+
 fn main() -> Result<(), anyhow::Error> {
+    // Load .env file if available for credentials and config
     dotenv()?;
-    env_logger::init();
+
+    // Set up logging, defaulting to "info" so we actually show some progress messages
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let args = Cli::parse();
 
     let path = Path::new(&args.filename);
 
     let out_path = args.output.map_or_else(
         // come up with a default output name
-        || {
-            path.file_stem()
-                .map(|p| {
-                    let mut p = p.to_owned();
-                    p.push(".updated.nbx");
-                    p
-                })
-                .map(|file_name| path.with_file_name(file_name))
-                .ok_or_else(|| anyhow!("Could not get file stem"))
-        },
+        || compute_default_output_filename(path),
         // or wrap our existing one in Ok
         Ok,
     )?;
 
-    println!("Connecting to GitLab: {}", &args.gitlab.gitlab_domain);
+    info!("Connecting to GitLab: {}", &args.gitlab.gitlab_domain);
 
     let gitlab =
         gitlab::GitlabBuilder::new(args.gitlab.gitlab_domain, args.gitlab.gitlab_access_token)
             .build()?;
+
+    info!(
+        "Setting up project mapper and querying default project {}",
+        &args.gitlab.default_project
+    );
     let mut mapper = ProjectMapper::new(gitlab, &args.gitlab.default_project)?;
     if let Some(default_formatting) = args.gitlab.default_project_format_as {
         mapper.try_set_project_name_formatting(None, &default_formatting)?;
     }
 
-    println!("Loading board from {}", path.display());
+    info!("Loading board from {}", path.display());
 
     let mut board = nullboard_tools::Board::load_from_json(path)?;
 
-    println!("Parsing notes");
+    info!("Parsing notes");
     let mut parsed_lists = vec![];
     // Parse all notes
     for list in board.take_lists() {
         parsed_lists.push(list.map_notes(parse_note));
     }
 
-    println!("Normalizing item references");
+    info!("Normalizing item references");
     let parsed_lists: Vec<_> = project_refs_to_ids(&mut mapper, parsed_lists).collect();
 
-    println!("Processing notes and associating with work units");
+    info!("Processing notes and associating with work units");
     let mut collection = WorkUnitCollection::default();
 
     let lists: Vec<_> =
         process_lists_and_associate_work_units(&mut collection, parsed_lists).collect();
 
-    println!("Pruning notes");
+    info!("Pruning notes");
     let lists = prune_notes(&mut collection, lists);
 
-    println!("Re-generating notes for export");
+    info!("Re-generating notes for export");
     let updated_board =
         board.make_new_revision_with_lists(map_note_data_in_lists(lists, |proc_note| {
             note_formatter::format_note(proc_note.into(), &mapper, |title| {
@@ -114,7 +127,7 @@ fn main() -> Result<(), anyhow::Error> {
             })
         }));
 
-    println!("Writing to {}", out_path.display());
+    info!("Writing to {}", out_path.display());
     updated_board.save_to_json(&out_path)?;
     Ok(())
 }
