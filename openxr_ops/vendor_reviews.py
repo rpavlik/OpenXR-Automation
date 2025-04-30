23f32902e2e498278acfd8fa4ed5d4a66f126019
Rylie Pavlik <rylie.pavlik@collabora.com>
2025-04-30 13:28:59 -0500

    Output for all vendors with pending extensions.

--- openxr_ops/vendor_reviews.py
+++ openxr_ops/vendor_reviews.py
@@ -7,6 +7,7 @@
 # Author: Rylie Pavlik <rylie.pavlik@collabora.com>
 
 import logging
+from pathlib import Path
 from typing import Iterable, List, Optional, Tuple, cast
 
 from .checklists import ColumnName, ReleaseChecklistCollection
@@ -98,12 +99,65 @@ def make_html(
         )
 
 
+def make_vendor_name_to_fn_map(
+    items: Iterable[ReleaseChecklistIssue], vendor_names_obj: VendorNames
+) -> dict[str, str]:
+    active_vendor_tags = set()
+    for item in items:
+        tag = item.vendor_tag
+        if tag is not None:
+            active_vendor_tags.add(vendor_names_obj.canonicalize_vendor_tag(tag))
+    fn_to_vendorname = {
+        f"vendor_{vendor_tag}.html": vendor_names_obj.get_vendor_name(vendor_tag)
+        for vendor_tag in active_vendor_tags
+    }
+    return {
+        vendor_name: filename
+        for filename, vendor_name in fn_to_vendorname.items()
+        if vendor_name is not None
+    }
+
+
+def filter_and_generate_vendor_page(
+    vendor_name: str,
+    filename: str,
+    sorted: Iterable[ReleaseChecklistIssue],
+    categories: dict[str, List[ReleaseChecklistIssue]],
+    extra: Optional[str] = None,
+    extra_safe: Optional[str] = None,
+):
+    log.info("Considering %s", vendor_name)
+
+    def filter_items(
+        data: Iterable[ReleaseChecklistIssue],
+    ) -> List[ReleaseChecklistIssue]:
+        return [x for x in data if x.vendor_name == vendor_name]
+
+    vendor_results = PriorityResults.from_sorted_items(filter_items(sorted))
+    vendor_categories = {
+        "needs_review": filter_items(categories["needs_review"]),
+        "needs_revision": filter_items(categories["needs_revision"]),
+        "needs_approval": filter_items(categories["needs_approval"]),
+    }
+
+    log.info("Outputting to HTML: %s", filename)
+    make_html(
+        vendor_name,
+        vendor_categories,
+        vendor_results,
+        sorter.get_sort_description(),
+        filename,
+        extra,
+        extra_safe,
+    )
+
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--html", type=str, help="Output HTML to filename")
+    parser.add_argument("--html-dir", type=str, help="Output HTML to directory")
     parser.add_argument("--extra", type=str, help="Extra text to add to HTML footer")
     parser.add_argument(
         "--config",
@@ -141,37 +195,18 @@ if __name__ == "__main__":
     sorter: SorterBase = config.get_sorter(vendor_names)
 
     sorted = sorter.get_sorted(categories["needs_review"])
-    for vendor_tag, vendor_data in config.vendor_config.items():
-        vendor_name = vendor_names.get_vendor_name(vendor_tag)
-        if not vendor_name:
-            log.warning("Could not look up vendor name for %s", vendor_tag)
-            continue
 
-        log.info("Considering %s", vendor_name)
-        outfilename = cast(Optional[str], vendor_data.get("output_filename"))
-        if not outfilename:
-            log.info("No vendor output file requested for %s", vendor_name)
-            continue
+    outdir = Path(".")
+    if args.html_dir:
+        outdir = Path(args.html_dir)
 
-        def filter_items(
-            data: List[ReleaseChecklistIssue],
-        ) -> List[ReleaseChecklistIssue]:
-            return [x for x in data if x.vendor_name == vendor_name]
-
-        vendor_results = PriorityResults.from_sorted_items(filter_items(sorted))
-        vendor_categories = {
-            "needs_review": filter_items(categories["needs_review"]),
-            "needs_revision": filter_items(categories["needs_revision"]),
-            "needs_approval": filter_items(categories["needs_approval"]),
-        }
-
-        log.info("Outputting to HTML: %s", outfilename)
-        make_html(
+    vendor_names_to_filenames = make_vendor_name_to_fn_map(items, vendor_names)
+    for vendor_name, filename in vendor_names_to_filenames.items():
+        filter_and_generate_vendor_page(
             vendor_name,
-            vendor_categories,
-            vendor_results,
-            sorter.get_sort_description(),
-            outfilename,
-            args.extra,
-            args.extra_safe,
+            str(outdir / filename),
+            sorted,
+            categories,
+            extra=args.extra,
+            extra_safe=args.extra_safe,
         )
