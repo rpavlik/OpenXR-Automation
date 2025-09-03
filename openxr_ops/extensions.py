02a58341da1ef6185e8b1e23746bce64c3f9839e
Rylie Pavlik <rylie.pavlik@collabora.com>
2025-09-03 12:48:44 -0500

    Update copyright dates

--- openxr_ops/extensions.py
+++ openxr_ops/extensions.py
@@ -1,5 +1,5 @@
 # Copyright 2022-2024, Collabora, Ltd.
-# Copyright 2024, The Khronos Group Inc.
+# Copyright 2024-2025, The Khronos Group Inc.
 #
 # SPDX-License-Identifier: BSL-1.0
 #
--- openxr_ops/review_data.py
+++ openxr_ops/review_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2022-2024, Collabora, Ltd.
-# Copyright 2024, The Khronos Group Inc.
+# Copyright 2024-2025, The Khronos Group Inc.
 #
 # SPDX-License-Identifier: BSL-1.0
 #
