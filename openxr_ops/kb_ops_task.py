a6cc5935fe610a870d63759e1a603518e94a1831
Rylie Pavlik <rylie.pavlik@collabora.com>
2025-11-20 14:27:37 -0600

    Missing copyright header

--- openxr_ops/kb_ops_task.py
+++ openxr_ops/kb_ops_task.py
@@ -1,3 +1,9 @@
+# Copyright 2022-2025, Collabora, Ltd.
+# Copyright 2024-2025, The Khronos Group Inc.
+#
+# SPDX-License-Identifier: BSL-1.0
+#
+# Author: Rylie Pavlik <rylie.pavlik@collabora.com>
 import datetime
 from dataclasses import dataclass
 from typing import Any
