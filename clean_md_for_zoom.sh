8f0a573e21b7947393f697d476a644dd2ccb91ca
Rylie Pavlik <rylie.pavlik@collabora.com>
2025-12-04 14:41:58 -0600

    Adjust clean_md_for_zoom.sh to handle output from new kanboard stuff

--- clean_md_for_zoom.sh
+++ clean_md_for_zoom.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Copyright 2024, Collabora, Ltd.
+# Copyright 2024-2025, Collabora, Ltd.
 #
 # SPDX-License-Identifier: BSL-1.0
 #
 # Author: Rylie Pavlik <rylie.pavlik@collabora.com>
 
 # Takes markdown links and just puts both the text and URL instead.
-# Good for copy/paste of nullboard_to_markdown.py output into Zoom meeting chat.
+# Good for copy/paste of `python3 -m openxr_ops.kb_cts.export` output into Zoom meeting chat.
 
-sed -i -E 's/\[([^]]+)\]\(([^)]+)\):/\1 \2/g' "$@"
+sed -i -E 's/\[([^]]+)\]\(([^)]+)\)/\1 \2/g' "$@"
